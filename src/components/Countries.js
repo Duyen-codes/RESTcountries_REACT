bdc92db28be536cc899df78398bde83fef7f480c
Duyen Nguyen <hongduyen0705@gmail.com>
2022-04-26 20:39:45 +0300

    created country component2

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,47 +1,31 @@
 import React, { Component } from "react";
+import Country from "./Country";
 
 const url = "https://restcountries.com/v3.1/all";
 
 class Countries extends Component {
   state = {
     countries: [],
+    isLoading: true,
   };
 
   componentDidMount() {
     fetch(url)
       .then((response) => response.json())
       .then((countries) => {
-        this.setState({ countries: countries });
+        this.setState({ countries: countries, isLoading: false });
       });
   }
   render() {
+    if (this.state.isLoading) {
+      return <p>Loading...</p>;
+    }
     return (
       <section className="cards">
         {this.state.countries.map((country) => {
           const { numericCode, name, population, region, capital, flags } =
             country;
-
-          return (
-            <div className="card">
-              <img className="flag" src={flags.svg} alt={name.official} />
-              <div className="content">
-                <h3 className="name">{name.official}</h3>
-                <div>
-                  <span className="country-info">Population: </span>
-                  <span className="population">{population}</span>
-                </div>
-                <div>
-                  <span className="country-info">Region: </span>
-                  <span className="region">{region}</span>
-                </div>
-
-                <div>
-                  <span className="country-info">Capital: </span>
-                  <span className="capital">{capital}</span>
-                </div>
-              </div>
-            </div>
-          );
+          return <Country country={country} key={numericCode} />;
         })}
       </section>
     );
