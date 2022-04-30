46781814b4e98da0da69fba0ea08830448914928
Duyen Nguyen <hongduyen0705@gmail.com>
2022-04-30 10:15:10 +0300

    add and style to-top button

---
+++ src/components/ToTopButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ToTopButton = () => {
+  return (
+    <button className="to-top-btn">
+      <i class="fa-solid fa-chevron-up"></i>
+    </button>
+  );
+};
+
+export default ToTopButton;
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import CountryList from "../components/CountryList";
 import Footer from "../components/Footer";
+import ToTopButton from "../components/ToTopButton";
 
 const Layout = () => {
   return (
@@ -9,6 +10,7 @@ const Layout = () => {
       <Header />
       <CountryList />
       <Footer />
+      <ToTopButton />
     </>
   );
 };
