b753061289852794a556fc9c07497a4000cfa0ca
Duyen Nguyen <hongduyen0705@gmail.com>
2022-08-15 10:00:43 +0300

    change routes

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import "./App.css";
 import CountryDetail from "./components/CountryDetail";
 import Layout from "./pages/Layout";
 import { useParams } from "react-router-dom";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const RouterWrapper = (props) => {
-  const params = useParams();
-  return <CountryDetail params={params} {...props} />;
-};
 
 function App() {
   return (
     <>
+      <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}></Route>
-        <Route path=":name" element={<RouterWrapper />} />
-      </Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path=":name" element={<CountryList />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
